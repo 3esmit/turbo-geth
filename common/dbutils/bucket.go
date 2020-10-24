147b439c8517b4ade78f8afe955e1fc42d24097f
alex.sharov <AskAlexSharov@gmail.com>
2020-10-24 07:39:06 +0700

    jump back

--- common/dbutils/bucket.go
+++ common/dbutils/bucket.go
@@ -61,6 +61,7 @@ var (
 	//key - address hash + incarnation + storage key hash
 	//value - storage value(common.hash)
 	CurrentStateBucket     = "CST2"
+	CurrentStateBucket3    = "CST3"
 	CurrentStateBucketOld1 = "CST"
 
 	//key - address hash
@@ -200,6 +201,7 @@ var (
 // BucketsConfigs - can be used to find index in sorted version of Buckets list by name
 var Buckets = []string{
 	CurrentStateBucket,
+	CurrentStateBucket3,
 	AccountsHistoryBucket,
 	StorageHistoryBucket,
 	CodeBucket,
@@ -313,6 +315,9 @@ type BucketConfigItem struct {
 }
 
 var BucketsConfigs = BucketsCfg{
+	CurrentStateBucket3: {
+		Flags: DupSort,
+	},
 	CurrentStateBucket: {
 		Flags:                     DupSort,
 		AutoDupSortKeysConversion: true,
